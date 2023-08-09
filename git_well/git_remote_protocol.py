2a6705463822c22e949f3abbd0e34105203b1a93
joncrall <erotemic@gmail.com>
2023-08-09 10:40:21 -0400

    Fix remote protocol

--- git_well/git_remote_protocol.py
+++ git_well/git_remote_protocol.py
@@ -9,9 +9,9 @@ class GitRemoteProtocol(scfg.DataConfig):
     group.
     """
     __command__ = 'remote_protocol'
-    group = scfg.Value(None, position=1, help='the group to change the protocol for')
-    repo_dpath = scfg.Value('.', position=2, help='location of the repo')
-    new_protocol = scfg.Value('git', help='protocol to change to')
+    group = scfg.Value('auto', position=1, help='the group to change the protocol for. If "auto", then attempts to determine.')
+    new_protocol = scfg.Value('git', help='protocol to change to', position=2)
+    repo_dpath = scfg.Value('.', help='location of the repo', position=3)
     # remote = scfg.Value(None, help='the remote to change')
 
 
@@ -19,14 +19,14 @@ def _parse_remote_url(url):
     info = {}
     if url.startswith('https://'):
         parts = url.split('https://')[1].split('/')
-        info['host'] = 'https://' + parts[0]
+        info['host'] = parts[0]
         info['group'] = parts[1]
         info['repo_name'] = parts[2]
         info['protocol'] = 'https'
     elif url.startswith('git@'):
         url.split('git@')[1]
         parts = url.split('git@')[1].split(':')
-        info['host'] = 'https://' + parts[0]
+        info['host'] = parts[0]
         info['group'] = parts[1].split('/')[0]
         info['repo_name'] = parts[1].split('/')[1]
         info['protocol'] = 'git'
@@ -44,27 +44,62 @@ def main(cmdline=1, **kwargs):
         >>> main(cmdline=cmdline, **kwargs)
     """
     config = GitRemoteProtocol.cli(cmdline=cmdline, data=kwargs, strict=True)
-    print('config = ' + ub.urepr(dict(config), nl=1))
+    import rich
+    rich.print('config = ' + ub.urepr(config, nl=1))
     from git_well._utils import find_git_root
     from git_well._repo_ext import Repo
     repo_root = find_git_root(config['repo_dpath'])
     repo = Repo(repo_root)
     repo.config_fpath
 
-    tasks = []
+    if config.new_protocol not in {'git', 'https'}:
+        raise KeyError(config.new_protocol)
+
+    remote_infos = []
     for remote in repo.remotes:
         for url in remote.urls:
             info = _parse_remote_url(url)
-            if info['protocol'] != config.new_protocol:
-                if config.group == info['group']:
+            info['url'] = url
+            remote_infos.append(info)
+
+    print('remote_infos = {}'.format(ub.urepr(remote_infos, nl=1)))
+
+    if config.group == 'auto':
+        print('Automatically determining group to change protocol for')
+        choices = list(ub.unique([r['group'] for r in remote_infos]))
+        if len(choices) == 1:
+            print('Only one choice')
+            config.group = choices[0]
+            print(f'Auto group: {config.group}')
+        else:
+            print('Multiple choices:')
+            import xdev
+            xdev.embed()
+            from rich.prompt import Prompt
+            ans = Prompt.ask('Which group to change protocol for?', choices=choices)
+            if not ans:
+                raise Exception
+            config.group = ans
+
+    tasks = []
+    for info in remote_infos:
+        if info['protocol'] != config.new_protocol:
+            if config.group == info['group']:
+                if config.new_protocol == 'git':
                     new_url = 'git@' + info['host']  + ':' + info['group'] + '/' + info['repo_name']
-                    tasks.append({
-                        'task': 'change',
-                        'old': url,
-                        'new': new_url,
-                    })
+                elif config.new_protocol == 'https':
+                    new_url = 'https://' + info['host']  + '/' + info['group'] + '/' + info['repo_name']
+                else:
+                    raise KeyError(config.new_protocol)
+                tasks.append({
+                    'task': 'change',
+                    'old': info['url'],
+                    'new': new_url,
+                })
 
     config_text = repo.config_fpath.read_text()
+    # print('tasks = {}'.format(ub.urepr(tasks, nl=1)))
+    print(f'Making {len(tasks)} changes')
     for task in tasks:
         config_text = config_text.replace(task['old'], task['new'])
     repo.config_fpath.write_text(config_text)
