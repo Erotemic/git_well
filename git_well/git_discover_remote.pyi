71fc5bc0f7a75f249568d6f4fa354ddfeb7b82e9
joncrall <erotemic@gmail.com>
2023-08-30 12:26:52 -0400

    Update xcookie and types

---
+++ git_well/git_discover_remote.pyi
@@ -0,0 +1,24 @@
+import scriptconfig as scfg
+from _typeshed import Incomplete
+
+
+class GitDiscoverRemoteCLI(scfg.DataConfig):
+    __command__: str
+    repo_dpath: Incomplete
+    host: Incomplete
+    remote: Incomplete
+    home: Incomplete
+    forward_ssh_agent: Incomplete
+    test_remote: Incomplete
+
+    @classmethod
+    def main(cls, cmdline: int = ..., **kwargs) -> None:
+        ...
+
+
+def fsspec_shh_connect(host):
+    ...
+
+
+__cli__ = GitDiscoverRemoteCLI
+main: Incomplete
